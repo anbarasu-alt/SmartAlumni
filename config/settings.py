16a78c757e16db5df58f439f3350a36774848bbd
anbarasu-alt <anbarasubsccs2006@gmail.com>
2026-09-14 20:36:31 +0530

    Fix Django WSGI configuration

--- config/settings.py
+++ config/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import dj_database_url
 import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -118,13 +119,13 @@ WSGI_APPLICATION = "config.wsgi.application"
 # =========================================================
 # DATABASE
 # =========================================================
-
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+    "default": dj_database_url.config(
+        conn_max_age=600,
+        default=os.getenv("postgresql://smartalumni_db_user:GqnO9lbaCbL357ewMvK5yIjScb5CwKHi@dpg-dak08lek1f9s739m5clg-a/smartalumni_db"),
+    )
+  }
+
 
 
 # =========================================================
